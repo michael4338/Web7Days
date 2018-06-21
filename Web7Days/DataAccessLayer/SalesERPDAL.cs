d76b123c0b619c560717c3c86043ecdae6b15628
Michael <michael4338@hotmail.com>
2018-06-20 18:51:07 -0700

    add new

--- Web7Days/DataAccessLayer/SalesERPDAL.cs
+++ Web7Days/DataAccessLayer/SalesERPDAL.cs
@@ -17,6 +17,6 @@ namespace Web7Days.DataAccessLayer
             Database.SetInitializer<SalesERPDAL>(null);
             modelBuilder.Entity<Employee>().ToTable("Tbl_Employee");
             base.OnModelCreating(modelBuilder);
-        }
+        } 
     }
 }
